026fbaf937fed76400e04d352651ee6275b5fbad
Joshua Seidel <jsseidel1@gmail.com>
2011-03-14 19:44:38 -0400

    SystemUI: Only add WiMAX icon slot if supported
    Change-Id: I4d419ad38a71f488b56552c72f252b9f7227992b

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java
@@ -431,6 +431,7 @@ public class StatusBarPolicy {
         mService = (StatusBarManager)context.getSystemService(Context.STATUS_BAR_SERVICE);
         mSignalStrength = new SignalStrength();
         mBatteryStats = BatteryStatsService.getService();
+	mWimaxSettingsHelper = new WimaxSettingsHelper(context);
 
         // storage
         mStorageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
@@ -464,9 +465,11 @@ public class StatusBarPolicy {
         // wifi will get updated by the sticky intents
 
         // wimax
-        mService.setIcon("wimax", sWimaxSignalImages[0][0], 0);
-        mService.setIconVisibility("wimax", false);
-        // wimax will get updated by the sticky intents
+	if (mWimaxSettingsHelper.isWimaxSupported()) {
+		mService.setIcon("wimax", sWimaxSignalImages[0][0], 0);
+		mService.setIconVisibility("wimax", false);
+		// wimax will get updated by the stickey intents
+	}
 
         // TTY status
         mService.setIcon("tty",  R.drawable.stat_sys_tty_mode, 0);
@@ -507,9 +510,6 @@ public class StatusBarPolicy {
         mService.setIcon("volume", R.drawable.stat_sys_ringer_silent, 0);
         mService.setIconVisibility("volume", false);
         updateVolume();
-
-	//wimax
-	mWimaxSettingsHelper = new WimaxSettingsHelper(context);
 
         IntentFilter filter = new IntentFilter();
 
