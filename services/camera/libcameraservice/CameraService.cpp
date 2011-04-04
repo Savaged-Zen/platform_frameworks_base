53c2e1cb04a42fd4fc95d2c6728c796788b78492
Christopher Lais <chris+android@zenthought.org>
2011-04-04 02:38:02 -0400

    libcameraservice: fix orientation on HTC FFC pictures
    Change-Id: I0bb0c4e795fc1c7965859a2802ceab92ea2e3a88

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1287,9 +1287,7 @@ void CameraService::Client::copyFrameAndPostCopiedFrame(
 }
 
 int CameraService::Client::getOrientation(int degrees, bool mirror) {
-#ifndef BOARD_USE_FROYO_LIBCAMERA
     if (!mirror) {
-#endif
         if (degrees == 0) return 0;
         else if (degrees == 90) return HAL_TRANSFORM_ROT_90;
         else if (degrees == 180) return HAL_TRANSFORM_ROT_180;
@@ -1305,6 +1303,13 @@ int CameraService::Client::getOrientation(int degrees, bool mirror) {
         } else if (degrees == 270) {  // FLIP_H and ROT_270
             return HAL_TRANSFORM_FLIP_V | HAL_TRANSFORM_ROT_90;
         }
+    }
+#else
+    } else {
+        if (degrees == 0) return HAL_TRANSFORM_ROT_180;
+        else if (degrees == 90) return HAL_TRANSFORM_ROT_270;
+        else if (degrees == 180) return 0;
+        else if (degrees == 270) return HAL_TRANSFORM_ROT_90;
     }
 #endif
     LOGE("Invalid setDisplayOrientation degrees=%d", degrees);
@@ -1394,7 +1399,7 @@ static const CameraInfo sCameraInfo[] = {
     },
     {
         CAMERA_FACING_FRONT,
-        270, /* orientation */
+        90, /* orientation */
     }
 };
 
