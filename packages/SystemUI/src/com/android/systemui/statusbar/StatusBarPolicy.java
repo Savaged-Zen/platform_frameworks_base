7ef51e52434039b7fb9dfaa6a2204f6ec0130195
Joshua Seidel <jsseidel1@gmail.com>
2011-04-19 07:01:28 -0500

    Revert "Revert "SystemUI: add theme compatibility for four bar signal""
    This reverts commit e9ea9dc19d7d3dbde38f52f477814539779a9a11.

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java
@@ -45,6 +45,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.storage.StorageManager;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
@@ -937,13 +938,21 @@ public class StatusBarPolicy {
             // asu = 0 (-113dB or less) is very weak
             // signal, its better to show 0 bars to the user in such cases.
             // asu = 99 is a special case, where the signal strength is unknown.
-            if (asu <= 2 || asu == 99) iconLevel = 0;
-            else if (asu >= 12) iconLevel = 6;
-            else if (asu >= 10) iconLevel = 5;
-            else if (asu >= 8) iconLevel = 4;
-            else if (asu >= 6)  iconLevel = 3;
-            else if (asu >= 4)  iconLevel = 2;
-            else iconLevel = 1;
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.THEME_COMPATIBILITY_SIGNAL, 0) == 0) { // Six bar
+                if (asu <= 2 || asu == 99) iconLevel = 0;
+                else if (asu >= 12) iconLevel = 6;
+                else if (asu >= 10) iconLevel = 5;
+                else if (asu >= 8) iconLevel = 4;
+                else if (asu >= 6)  iconLevel = 3;
+                else if (asu >= 4)  iconLevel = 2;
+                else iconLevel = 1;
+            } else {  // Four bar
+                if (asu <= 2 || asu == 99) iconLevel = 0;
+                else if (asu >= 12) iconLevel = 4;
+                else if (asu >= 8)  iconLevel = 3;
+                else if (asu >= 5)  iconLevel = 2;
+            }
 
             // Though mPhone is a Manager, this call is not an IPC
             if (mPhone.isNetworkRoaming()) {
@@ -976,8 +985,10 @@ public class StatusBarPolicy {
         int levelDbm = 0;
         int levelEcio = 0;
         
-       /*
-         * HTC's signal to icon
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.THEME_COMPATIBILITY_SIGNAL, 0) == 0) { // Six bar
+           /*
+             * HTC's signal to icon
 
 			0 -> anything worse than -110
 			1 -> -110
@@ -986,18 +997,18 @@ public class StatusBarPolicy {
 			4 -> -95
 			5 -> -85
 			6 -> -75
-         */
+             */
 
-        if (cdmaDbm >= -75) levelDbm = 6;
-        else if (cdmaDbm >= -85) levelDbm = 5;
-        else if (cdmaDbm >= -95) levelDbm = 4;
-        else if (cdmaDbm >= -100) levelDbm = 3;
-        else if (cdmaDbm >= -105) levelDbm = 2;
-        else if (cdmaDbm >= -110) levelDbm = 1;
-        else levelDbm = 0;
+            if (cdmaDbm >= -75) levelDbm = 6;
+            else if (cdmaDbm >= -85) levelDbm = 5;
+            else if (cdmaDbm >= -95) levelDbm = 4;
+            else if (cdmaDbm >= -100) levelDbm = 3;
+            else if (cdmaDbm >= -105) levelDbm = 2;
+            else if (cdmaDbm >= -110) levelDbm = 1;
+            else levelDbm = 0;
 
-        /*
-         * HTC's signal to icon
+            /*
+             * HTC's signal to icon
 
 			0 -> anything worse than -150
 			1 -> -150
@@ -1006,16 +1017,30 @@ public class StatusBarPolicy {
 			4 -> -120
 			5 -> -110
 			6 -> -90
-         */
+             */
 
-        // Ec/Io are in dB*10
-        if (cdmaEcio >= -90) levelEcio = 6;
-        else if (cdmaEcio >= -110) levelEcio = 5;
-        else if (cdmaEcio >= -120) levelEcio = 4;
-        else if (cdmaEcio >= -130) levelEcio = 3;
-        else if (cdmaEcio >= -140) levelEcio = 2;
-        else if (cdmaEcio >= -150) levelEcio = 1;
-        else levelEcio = 0;
+            // Ec/Io are in dB*10
+            if (cdmaEcio >= -90) levelEcio = 6;
+            else if (cdmaEcio >= -110) levelEcio = 5;
+            else if (cdmaEcio >= -120) levelEcio = 4;
+            else if (cdmaEcio >= -130) levelEcio = 3;
+            else if (cdmaEcio >= -140) levelEcio = 2;
+            else if (cdmaEcio >= -150) levelEcio = 1;
+            else levelEcio = 0;
+        } else { // Four bar
+            if (cdmaDbm >= -75) levelDbm = 4;
+            else if (cdmaDbm >= -85) levelDbm = 3;
+            else if (cdmaDbm >= -95) levelDbm = 2;
+            else if (cdmaDbm >= -100) levelDbm = 1;
+            else levelDbm = 0;
+
+            // Ec/Io are in dB*10
+            if (cdmaEcio >= -90) levelEcio = 4;
+            else if (cdmaEcio >= -110) levelEcio = 3;
+            else if (cdmaEcio >= -130) levelEcio = 2;
+            else if (cdmaEcio >= -150) levelEcio = 1;
+            else levelEcio = 0;
+        }
 
         return (levelDbm < levelEcio) ? levelDbm : levelEcio;
     }
@@ -1026,22 +1051,36 @@ public class StatusBarPolicy {
         int levelEvdoDbm = 0;
         int levelEvdoSnr = 0;
 
-        if (evdoDbm >= -75) levelEvdoDbm = 6;
-        else if (evdoDbm >= -85) levelEvdoDbm = 5;
-        else if (evdoDbm >= -90) levelEvdoDbm = 4;
-        else if (evdoDbm >= -95) levelEvdoDbm = 3;
-        else if (evdoDbm >= -100) levelEvdoDbm = 2;
-        else if (evdoDbm >= -105) levelEvdoDbm = 1;
-        else levelEvdoDbm = 0;
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.THEME_COMPATIBILITY_SIGNAL, 0) == 0) { // Six bar
+            if (evdoDbm >= -75) levelEvdoDbm = 6;
+            else if (evdoDbm >= -85) levelEvdoDbm = 5;
+            else if (evdoDbm >= -90) levelEvdoDbm = 4;
+            else if (evdoDbm >= -95) levelEvdoDbm = 3;
+            else if (evdoDbm >= -100) levelEvdoDbm = 2;
+            else if (evdoDbm >= -105) levelEvdoDbm = 1;
+            else levelEvdoDbm = 0;
 
-        if (evdoSnr >= 8) levelEvdoSnr = 6;
-        else if (evdoSnr >= 6) levelEvdoSnr = 5;
-        else if (evdoSnr >= 5) levelEvdoSnr = 4;
-        else if (evdoSnr >= 4) levelEvdoSnr = 3;
-        else if (evdoSnr >= 3) levelEvdoSnr = 2;
-        else if (evdoSnr >= 1) levelEvdoSnr = 1;
-        else levelEvdoSnr = 0;
+            if (evdoSnr >= 8) levelEvdoSnr = 6;
+            else if (evdoSnr >= 6) levelEvdoSnr = 5;
+            else if (evdoSnr >= 5) levelEvdoSnr = 4;
+            else if (evdoSnr >= 4) levelEvdoSnr = 3;
+            else if (evdoSnr >= 3) levelEvdoSnr = 2;
+            else if (evdoSnr >= 1) levelEvdoSnr = 1;
+            else levelEvdoSnr = 0;
+        } else { // Four bar
+            if (evdoDbm >= -65) levelEvdoDbm = 4;
+            else if (evdoDbm >= -75) levelEvdoDbm = 3;
+            else if (evdoDbm >= -90) levelEvdoDbm = 2;
+            else if (evdoDbm >= -105) levelEvdoDbm = 1;
+            else levelEvdoDbm = 0;
 
+            if (evdoSnr >= 7) levelEvdoSnr = 4;
+            else if (evdoSnr >= 5) levelEvdoSnr = 3;
+            else if (evdoSnr >= 3) levelEvdoSnr = 2;
+            else if (evdoSnr >= 1) levelEvdoSnr = 1;
+            else levelEvdoSnr = 0;
+        }
         return (levelEvdoDbm < levelEvdoSnr) ? levelEvdoDbm : levelEvdoSnr;
     }
 
